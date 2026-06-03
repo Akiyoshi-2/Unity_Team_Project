100869b1ed806b345cb5e0ad18e0a34354fe6b24
TanakaYuya <ktq2590331@stu.o-hara.ac.jp>
2026-06-03 15:12:01 +0900

    画像追加

--- Assets/Scripts/Kawakami/Title.cs
+++ Assets/Scripts/Kawakami/Title.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+
 
 }
