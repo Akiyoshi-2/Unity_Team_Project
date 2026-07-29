bcf8bbf704bd3c0e3f90ebbc4b40296003f5c393
syo-ya <wzg2q9tpfw@privaterelay.appleid.com>
2026-07-29 10:29:51 +0900

    compass

--- Assets/Scripts/TANAKA/compass.cs
+++ Assets/Scripts/TANAKA/compass.cs
@@ -1,16 +1,52 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class compass : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public enum PositionType
     {
-        
+        Position,
+        Object
     }
 
-    // Update is called once per frame
+    [SerializeField]
+    private PositionType positionType;
+
+    [SerializeField]
+    private Vector3 targetPosition;
+
+    [SerializeField]
+    private GameObject targetObject;
+
+    private Vector3 targetPos;
+
+    private Transform m_Player;
+
+    private void Start()
+    {
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player != null) m_Player = player;
+    }
+
     void Update()
     {
-        
+        if (positionType == PositionType.Position)
+        {
+            targetPos = targetPosition;
+        }
+        else if (positionType == PositionType.Object)
+        {
+            targetPos = targetObject.transform.position;
+        }
+
+        Vector3 dir = targetPos - m_Player.position;
+
+        dir.y = 0;
+
+        float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+
+        float angle = targetAngle - m_Player.localEulerAngles.y;
+
+        this.transform.localRotation = Quaternion.Euler(0, 0, -angle);
     }
 }
