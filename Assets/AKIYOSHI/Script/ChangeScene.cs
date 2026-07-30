93435c908b11ba84b28db21832a80c874e59a1d5
bioshock <shingo20050302@au.comgit config --global user.name bioshock git config --global user.email shingo20050302@au.com>
2026-07-30 11:51:42 +0900

    FadeIn、FadeOut実装

--- Assets/AKIYOSHI/Script/ChangeScene.cs
+++ Assets/AKIYOSHI/Script/ChangeScene.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public string nextSceneName; // à⁄ìÆêÊÇÃÉVÅ[Éìñº
+    public string nextSceneName;
 
     private Player player;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
@@ -13,19 +13,19 @@ public class ChangeScene : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (nextSceneName == "GoalScene")
         {
-            if (nextSceneName == "GoalScene")
+            if (player.GetGoalItemFlg())
             {
-                if (player.GetGoalItemFlg())
-                {
-                    SceneManager.LoadScene(nextSceneName);
-                }
+                FadeManager.Instance.LoadScene(nextSceneName);
             }
-            else
-            {
-                SceneManager.LoadScene(nextSceneName);
-            }
+        }
+        else
+        {
+            FadeManager.Instance.LoadScene(nextSceneName);
         }
     }
 }
---
+++ Assets/AKIYOSHI/Script/FadeManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FadeManager : MonoBehaviour
+{
+    public static FadeManager Instance;
+
+    [Header("Fade")]
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeOutTime = 1.2f;
+    [SerializeField] private float fadeInTime = 1.5f;
+    [SerializeField] private float blackWaitTime = 0.3f;
+
+    private bool isFading = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Fade(1f, 0f, fadeInTime));
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        if (!isFading)
+        {
+            StartCoroutine(FadeRoutine(sceneName));
+        }
+    }
+
+    IEnumerator FadeRoutine(string sceneName)
+    {
+        isFading = true;
+
+        yield return Fade(0f, 1f, fadeOutTime);
+
+        yield return new WaitForSeconds(blackWaitTime);
+
+        yield return SceneManager.LoadSceneAsync(sceneName);
+
+        yield return new WaitForSeconds(blackWaitTime);
+
+        yield return Fade(1f, 0f, fadeInTime);
+
+        isFading = false;
+    }
+
+    IEnumerator Fade(float start, float end, float time)
+    {
+        float t = 0;
+        Color color = fadeImage.color;
+
+        while (t < time)
+        {
+            t += Time.deltaTime;
+
+            float x = Mathf.Clamp01(t / time);
+
+            // なめらかなイージング
+            x = Mathf.SmoothStep(0f, 1f, x);
+
+            color.a = Mathf.Lerp(start, end, x);
+
+            fadeImage.color = color;
+
+            yield return null;
+        }
+
+        color.a = end;
+        fadeImage.color = color;
+    }
+}
